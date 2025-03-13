2c1d36572242904a44e208047d61990c041f00a7
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-03-13 09:24:58 -0400

    vanlizey

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from "react";
 
-export default function Review(){
-    return <>
-             <div className='rev-main-div'>
-                <div className='rev-chart-div'>
-                    <img src='/rev-chart.png' />
-                </div>
-                <div className='star-div'>
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                </div>
-                <div className='rev-1-div'>
-                    <img src='/rev-1.png' />
-                </div>
-                 <div className='star-div'>
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                    <img src='/star.png' />
-                </div>
-                <div className='rev-2-div'>
-                    <img src='/rev-2.png' />
-                </div>
-             </div>
-            </>
+export default function Review() {
+  return (
+    <>
+      <div className="rev-main-div">
+        <div className="rev-chart-div">
+          <img src="/src/images/rev-chart.png" />
+        </div>
+        <div className="star-div">
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+        </div>
+        <div className="rev-1-div">
+          <img src="/src/images/rev-1.png" />
+        </div>
+        <div className="star-div">
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+          <img src="/src/images/star.png" />
+        </div>
+        <div className="rev-2-div">
+          <img src="/src/images/rev-2.png" />
+        </div>
+      </div>
+    </>
+  );
 }
