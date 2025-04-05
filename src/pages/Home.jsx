9e24618b71f16e857ef4b9843a0749cb7358acb6
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-04-05 11:11:33 -0400

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Home(){
-  return <div className='home-body'>
-  <div className='home-header'>You Got The Travel Plans -- We Got The Vans</div>
-  <div className='home-subHeader'>Add aventure to your life by joining the #vanlife movement. Rent your perfect van to have the perfect road trip!</div>
-  <Link className='home-button-link' to='/vans'><button className='home-button'>Find Your Van</button></Link>
-  </div>
-};
+export default function Home() {
+  return (
+    <div className="home-body">
+      <div className="home-header">You Got The Travel Plans -- We Got The Vans</div>
+      <div className="home-subHeader">Add adventure to your life by joining the #vanlife movement. Rent your perfect van to have the perfect road trip!</div>
+      <Link className="home-button-link" to="/vans">
+        <button className="home-button">Find Your Van</button>
+      </Link>
+    </div>
+  );
+}
